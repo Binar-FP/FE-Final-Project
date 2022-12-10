1a59c393d76f95bbccbc85ed4260c7a376fbb0dd
Fathan Mubarok <falonez01@gmail.com>
2022-12-10 14:36:06 +0700

    implementation Redux

---
+++ src/config/redux/actions/authActions.js
@@ -0,0 +1,14 @@
+import { AuthService } from "../../../services/authService";
+import SweatAlert from "../../SweatAlert";
+
+export const loginActions = (data, history) => async (dispatch) => {
+    try {
+        const response = await AuthService.login(data);
+        dispatch({type: 'LOGIN', payload: response.data});
+        SweatAlert('Login Berhasil', 'success');
+        history('/');
+    } catch (error) {
+        SweatAlert(String(error.response.data.message), 'warning')
+    }   
+}
+
