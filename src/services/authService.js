ea1179c7473d86d266cd916bbac544eed0c30823
Fathan Mubarok <falonez01@gmail.com>
2022-12-10 14:38:59 +0700

    Add Services and seting pages

---
+++ src/services/authService.js
@@ -0,0 +1,31 @@
+import API from "./api"
+
+export const AuthService = {
+    login : async (data) => {
+        const response = await API.post('/login', data);
+        setHeadersAndStorage(response.data, data.rememberMe);
+        if (data.rememberMe === true) {
+            setTimeout(() => {
+                localStorage.removeItem('token');
+                localStorage.removeItem('isLogged');
+            }
+            , 604800000);
+        } else {
+            setTimeout(() => {
+                localStorage.removeItem('token');
+                localStorage.removeItem('isLogged');
+            }
+            , 86400000);
+        }
+        return response;
+    }
+}
+
+const setHeadersAndStorage = ({ user, token}, rememberMe) => {
+    API.defaults.headers['Authorization'] = `Bearer ${token}`;
+    // localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', token);
+    localStorage.setItem('isLogged',true);
+    
+    
+  }
