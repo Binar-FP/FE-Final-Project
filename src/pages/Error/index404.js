cd163115e58c5b95d29ad3d2bd9b815b0dca00d5
Fathan Mubarok <falonez01@gmail.com>
2022-12-11 22:02:59 +0700

    fix eslint

--- src/pages/Error/index404.js
+++ src/pages/Error/index404.js
@@ -1,11 +1,11 @@
 import React from 'react'
 // import { Navbar, Error_Handler_404, Footer } from '../../components'
-import { Error_Handler_404 } from '../../components'
+import { ERROR_HANDLER_404 } from '../../components'
 
 const Error404 = () => {
   return (
     <div>
-      <Error_Handler_404 />
+      <ERROR_HANDLER_404 />
     </div>
   )
 }
--- src/pages/Error/index500.js
+++ src/pages/Error/index500.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Error_Handler_500 } from '../../components'
+import { ERROR_HANDLER_500 } from '../../components'
 
 const Error500 = () => {
   return (
     <div>
-      <Error_Handler_500 />
+      <ERROR_HANDLER_500 />
     </div>
   )
 }
