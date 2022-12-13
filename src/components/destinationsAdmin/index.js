9d4bbb8e36d8624fa27b85db49413ae40b15214c
Fathan Mubarok <falonez01@gmail.com>
2022-12-13 17:04:28 +0700

    Create DestinationsAdmin

---
+++ src/components/destinationsAdmin/index.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from 'react'
+import './destinationsAdmin.css'
+import { PencilSquare, PlusCircle, Trash, } from 'react-bootstrap-icons'
+import { getDestinationsActions, PutDestinationsActions, DeleteDestinationsActions, CreateDestinationsActions } from '../../config/redux/actions/destinationsActions';
+import { useDispatch } from 'react-redux';
+
+const DestinationsAdmin = () => {
+    const [destinations, setDestinations] = useState([])
+    const [formValues, setFormValues] = useState({userId: 1})
+    const dispatch = useDispatch();
+    
+
+    const getDestinations = async () => {
+        const data = await dispatch(getDestinationsActions());
+        setDestinations(data.data.data)
+    }
+    
+    useEffect(() => {
+        getDestinations()
+    }, [])
+    
+    console.log(formValues)
+    const updatehandler = async () => {
+        await dispatch(PutDestinationsActions(formValues.id,formValues));
+        // window.location.reload(true);
+    }
+
+    const deleteHandler = async (id) => {
+        await dispatch(DeleteDestinationsActions(id));
+        window.location.reload(false);
+    }
+
+    const createHandler = async () => {
+        await dispatch(CreateDestinationsActions(formValues));
+        // window.location.reload(true);
+    }
+
+  return (
+    <>
+      <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 className="h2">Destinations</h1>
+        <div className="btn-toolbar mb-2 mb-md-0">
+          <div className="btn-group me-2">
+            <button type="button" className="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#createModal"><PlusCircle className='me-2'/>Add</button>
+          </div>
+        </div>
+      </div>
+      {/* <h2>Section title</h2> */}
+          <div className="table-responsive">
+            <table className="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Name Destination</th>
+                  <th scope="col">Image</th>
+                  <th scope="col">Edit</th>
+                  <th scope="col">Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                {destinations.map((destinations) => {
+                return (
+                <>
+                    <tr>
+                    <td>{destinations.id}</td>
+                    <td>{destinations.nameDestination}</td>
+                    <td><img className='img-destinations' alt='' src={destinations.imageDestination} /></td>
+                    {/* MODAL */}
+                        {/* <!-- Button trigger modal --> */}
+                        <td><PencilSquare data-bs-toggle="modal" onClick={(e)=> setFormValues({...formValues,nameDestination: destinations.nameDestination, id :destinations.id, imageDestination: destinations.imageDestination})} data-bs-target={`#example${destinations.id}`} size={20}  /></td>
+                        <td><Trash onClick={()=>deleteHandler(destinations.id)} size={20}  /></td>
+                    </tr>
+
+                    {/* <!-- Modal --> */}
+                    <div className="modal fade" id={`example${destinations.id}`} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Update Destinations</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                            <form className="">
+                                <div className="modal-body">
+                                        <label htmlFor="" className="mb-2">Name Destinations</label>
+                                        <input defaultValue={destinations.nameDestination} onChange={(e)=> setFormValues({...formValues,nameDestination: e.target.value, id :destinations.id})} className="form-control" name='namnameDestinatione' type="text"/>
+                                        <label htmlFor="" className="mb-2">Image Destination</label>
+                                        {/* <img className='img-destinations' src={destinations.imageDestination} alt=""/> */}
+                                        <input onChange={(e)=> setFormValues({...formValues,imageDestination: e.target.files[0], id :destinations.id})} className="form-control" name='imageDestination' type="file"/>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={updatehandler}>Save changes</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    </div>
+                </>
+              )
+            })}
+              </tbody>
+            </table>
+            {/* <!-- Modal Create --> */}
+              <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Create destinations</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <form className="">
+                      <label htmlFor="" className="mb-2">Name</label>
+                      <input placeholder='Name destinations' onChange={(e)=> setFormValues({...formValues,nameDestination: e.target.value, id :destinations.id})} className="form-control" name='name' type="text"/>
+                      <label htmlFor="" className="mb-2">Code</label>
+                      <input placeholder='Image destinations'onChange={(e)=> setFormValues({...formValues,imageDestination: e.target.files[0], id :destinations.id})} className="form-control" name='code' type="file"/>
+                      </form>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={createHandler}>Create</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+    </main>
+    </>
+  )
+}
+
+export default DestinationsAdmin
