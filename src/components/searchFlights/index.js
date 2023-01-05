9110e1e48288bdf3892ee61f4a1c57bf825b28f7
Fathan Mubarok <falonez01@gmail.com>
2023-01-05 22:26:26 +0700

    Update Logic Check Login in Search Flight

--- src/components/searchFlights/index.js
+++ src/components/searchFlights/index.js
@@ -56,19 +56,25 @@ const SearchFlights = (props) => {
     }, [props.data,dispatch])
 
     useEffect(() => {
-        if (isLoggedIn === false) {
-            history('/login');
-        }else{
+        
 
             if (checkTrue === true && checkTrueRound === true) {
+                if (isLoggedIn === false) {
+                    history('/login');
+                }else{
                 dispatch({type: 'CONFIRM_FLIGHT', payload: formValues});
                 dispatch({type: 'CONFIRM_FLIGHT_ROUND', payload: formValuesRound});
                 history('/booking');
+                }
             }else if (checkTrue === true && props.data.typeOfFlight === 'One Way') {
+                if (isLoggedIn === false) {
+                    history('/login');
+                }else{
                 dispatch({type: 'CONFIRM_FLIGHT', payload: formValues});
                 history('/booking');
+                }
             }
-        }
+        
         
     }, [checkTrue, checkTrueRound, history, dispatch, formValues, formValuesRound,props.data,isLoggedIn])
 
